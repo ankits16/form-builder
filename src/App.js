50a2f8392de4c1cae140cff4ec5345581225e354
Ankit Sachan <>
2023-03-15 19:06:50 +0530

    removing drag ui test code

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
   return (
     <>
       <FormBuilderHeader />
-      <DragUI></DragUI>
+      {/* <DragUI></DragUI> */}
       <Canvas formData={formData} formOperation={updateFormDataFromUI} />
       
     </>
